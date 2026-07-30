01ee47fea36b28c3b06cbb32f76a9c44cc631e42
sitorasu <13363542+sitorasu@users.noreply.github.com>
2026-07-30 21:02:26 +0900

    Add script to run test

---
+++ run_test.ps1
@@ -0,0 +1,22 @@
+﻿$unity = "C:\Program Files\Unity\Hub\Editor\2022.3.22f1\Editor\Unity.exe"
+$project = "C:\Users\sitor\MyFolder\test-avatar-proj"
+$result = Join-Path $PSScriptRoot "TestResults\editmode.xml"
+$log = Join-Path $PSScriptRoot "TestResults\editmode.log"
+
+New-Item -ItemType Directory -Force (Split-Path $result) | Out-Null
+
+$arguments = @(
+    "-batchmode"
+    "-runTests"
+    "-projectPath", $project
+    "-testPlatform", "EditMode"
+    "-testFilter", "Sitorasu.MaterialAssignmentTransfer"
+    "-testResults", $result
+    "-logFile", $log
+)
+
+$process = Start-Process -FilePath $unity -ArgumentList $arguments -PassThru -Wait -NoNewWindow
+
+$exitCode = $process.ExitCode
+Write-Host "Unity exit code: $exitCode"
+exit $exitCode
